0d8af01d1086cf2706d2a027e80f37d2834c8796
gsandhu916 <gsandhu916@gmail.com>
2019-09-04 18:23:06 -0700

    content box created

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,10 +3,26 @@ import React from "react";
 const Content = () => {
   return (
     <div className="content-container">
+      <div className="content-statement">
       <p>
         Fully has the best desks, chairs, and other tools to keep you moving,
         engaged, and loving how you feel at work.
       </p>
+      </div>
+      <div className="content-statement2">
+        <ul>
+          <li className="li-statement"><h1>Desks to keep you moving</h1>
+          <p>The best collection of adjustable height desks, including our stable, sustainable, and customizable Jarvis.</p>
+          <button>Shop Desks</button>
+          </li>
+          <li>
+            <picture>
+            <img className="img1" src="/images/statement/statement2img1.png" alt="people around desk"/>
+          <img className="img2" src="/images/statement/statement2img2.png" alt="people around desk" />
+          </picture>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
